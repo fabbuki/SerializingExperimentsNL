bec684fe37725c49d1045c9fe6fcb3594fbbaf1d
Nick Lee <nickaustinlee@gmail.com>
2014-03-02 00:54:53 -0800

    Added a serializer to the note class

--- SerializingTest/SerializingTest/Class1.cs
+++ SerializingTest/SerializingTest/Class1.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization; 
 
 namespace SerializingTest
 {
     using System;
     public class Note
     {
-        string title;
-        string body;
-        string dateCreated;
-        string lastModified;
+       public string title;
+       public string body;
+       public string dateCreated;
+       public string lastModified;
 
         public Note()
         {
@@ -26,10 +28,30 @@ namespace SerializingTest
 
         public void outputNote()
         {
-            Console.WriteLine("Titlte: {0}", title);
+            Console.WriteLine("Title: {0}", title);
             Console.WriteLine("Body: {0}", body);
             Console.WriteLine("Date Created: {0}", dateCreated);
             Console.WriteLine("Date last modified: {0}", lastModified);
+        }
+
+        public string SerializeXMLReturnURL()
+        {
+
+            //create the serializer 
+            XmlSerializer xs = new XmlSerializer(typeof(Note)); 
+
+            //create file URL myPath 
+            string myPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            myPath = Path.Combine(myPath, "SerializationOverview.xml"); 
+            
+            //test
+            this.outputNote(); 
+
+            //serialize yourself and close file 
+            using (Stream s = File.Create(myPath))
+                xs.Serialize(s, this); 
+            
+            return myPath; 
         }
 
     }
--- SerializingTest/SerializingTest/Program.cs
+++ SerializingTest/SerializingTest/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization; 
 
 namespace SerializingTest
 {
@@ -13,7 +15,28 @@ namespace SerializingTest
             Note myNote = new Note();
             myNote.outputNote();
 
-            Console.WriteLine("Press any key to exit."); 
+            Console.WriteLine("Press a key to move to next phase."); 
+            Console.ReadKey(); 
+
+            //serialize the object with inception 
+            string theURL = myNote.SerializeXMLReturnURL(); 
+            Console.WriteLine("The serialized XML file should be located at {0}", theURL); 
+
+            Console.WriteLine("Press a key to move to next phase.");
+            Console.ReadKey(); 
+
+            //de-serialize the object
+            Note noteClone; 
+            XmlSerializer xs = new XmlSerializer(typeof(Note));
+
+            using (Stream s = File.OpenRead(theURL))
+                noteClone = (Note)xs.Deserialize(s); 
+
+            //output the object 
+            Console.WriteLine("I have de-serialized and cloned the note. This is the note's properties:");
+            noteClone.outputNote();
+
+            Console.WriteLine("Press a key to move to next phase.");
             Console.ReadKey(); 
 
         }
