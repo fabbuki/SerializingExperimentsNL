4b05f26ad4103c3ccfad73ba9cd435d117b22321
Nick Lee <nickaustinlee@gmail.com>
2014-03-02 00:04:01 -0800

    Created a Note Class
    I gave the note class some basic properties. Nothing special here.

---
+++ SerializingTest/SerializingTest/Note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Note
+{
+    string title;
+    string body;
+    string dateCreated;
+    string lastModified; 
+
+	public Note()
+	{
+        title = "Untitled";
+        body = "This is a test note.";
+
+        dateCreated = DateTime.Today.ToString("MMMM dd, yyyy");
+        lastModified = DateTime.Today.ToString("MMMM dd, yyyy");
+
+    }
+    
+    public void outputNote() 
+    {
+        Console.WriteLine("Titlte: {0}", title); 
+        Console.WriteLine("Body: {0}", body); 
+        Console.WriteLine("Date Created: {0}", dateCreated); 
+        Console.WriteLine("Date last modified: {0}", lastModified); 
+    }
+
+}
